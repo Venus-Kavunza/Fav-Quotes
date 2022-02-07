491ed4c7e7c5934206f9fbc6743b469f75e9ad86
Venus-Kavunza <venusmwende@gmail.com>
2022-02-07 20:48:26 +0300

    it will all a quote class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quotesapp';
+  quotes:string[];
+
+  constructor(){
+    this.quotes = ['Surround yourself with positive people and you’ll be a positive person.', 'It’s almost always possible to be honest and positive.', 'I surround myself with positive, productive people of good will and decency.', 
+    ]
+  }
 }
