8260954d0a935201f431607d67f5b4950c14f297
Venus-Kavunza <venusmwende@gmail.com>
2022-02-07 22:44:16 +0300

    it will add the changes to quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:Quote[] = [ 
-    {quote:'I surround myself with positive, productive people of good will and decency.', arthur:'Ted Nugent', name:'Venus Mwende'},
-    {quote:'Keep your face to the sunshine and you cannot see a shadow.', arthur:'Helen Keller', name:'Venus Mwende'},
-    {quote:'The positive thinker sees the invisible, feels the intangible, and achieves the impossible.', arthur:'Winston Churchill', name:'Venus Mwende'}
-  ]
+  
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[] = [ 
+    {quote:'I surround myself with positive, productive people of good will and decency.', arthur:'Ted Nugent', name:'Venus Mwende'},
+    {quote:'Keep your face to the sunshine and you cannot see a shadow.', arthur:'Helen Keller', name:'Venus Mwende'},
+    {quote:'The positive thinker sees the invisible, feels the intangible, and achieves the impossible.', arthur:'Winston Churchill', name:'Venus Mwende'}
+  ]
 
   constructor() { }
 
