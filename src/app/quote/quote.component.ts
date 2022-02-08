9ea75e949a317c458b786b4de7f972ca4b2411e5
Venus-Kavunza <venusmwende@gmail.com>
2022-02-08 16:58:57 +0300

    It will add changes to delete button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -17,9 +17,13 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index:any){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  completeQuote(isComplete:any, index:any){
+  deleteQuote(isComplete:any, index:any){
     if (isComplete) {
+      let toDelete = confirm('Are you sure you want to delete ${this.quotes{index}.name}?')
+
+      if (toDelete){
       this.quotes.splice(index,1);
+      }
     }
   }
   constructor() { }
