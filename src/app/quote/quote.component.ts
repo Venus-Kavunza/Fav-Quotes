b13c4b4d69a59090ce73363a22ebb83b1f942a0c
Venus-Kavunza <venusmwende@gmail.com>
2022-02-08 11:00:50 +0300

    It will add an eventemitter

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -17,6 +17,11 @@ export class QuoteComponent implements OnInit {
   toggleDetails(index:any){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+  completeQuote(isComplete:any, index:any){
+    if (isComplete) {
+      this.quotes.splice(index,1);
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
